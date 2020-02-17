c5d361c84935b557394bccaa7aa2bdb002d2c3e5
Noah Gambee <noah@gambee.dev>
2020-02-17 13:10:14 -0700

    Add boilderplate to container

---
+++ src/ReservationsContainer/ReservationContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+// import ReservationCard from '../ReservationCard/ReservationCard';
+
+const ReservationContainer = (props) => {
+
+}
+
+export default ReservationContainer;
