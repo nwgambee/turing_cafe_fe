83f89d795ca69dfee83fca7aa7dcca49ff0fc720
Noah Gambee <noah@gambee.dev>
2020-02-17 15:24:48 -0700

    Add import statements

---
+++ src/ReservationCard/ReservationCard.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReservationCard from './ReservationCard';
+import {shallow} from 'enzyme';
---
+++ src/ReservationsContainer/ReservationContainer.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReservationContainer from './ReservationContainer';
+import {shallow} from 'enzyme';
