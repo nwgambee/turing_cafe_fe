78a4ec70a583e43751fc24d44065464c680a3560
Noah Gambee <noah@gambee.dev>
2020-02-17 15:28:54 -0700

    Add snapshot test for ReservationCard

--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -2,3 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReservationCard from './ReservationCard';
 import {shallow} from 'enzyme';
+
+it('should match the snapshot', () => {
+  let wrapper = shallow(<ReservationCard
+    name='Noah'
+    id={123}
+    date='12/13'
+    time='3:45'
+    number={4}
+    cancelReservation={jest.fn()}
+    />)
+  expect(wrapper).toMatchSnapshot();
+})
