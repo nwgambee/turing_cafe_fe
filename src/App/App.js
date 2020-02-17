0c358c9fb718bb4be94836169382ad2290e77fc4
Noah Gambee <noah@gambee.dev>
2020-02-17 13:02:58 -0700

    Add constructor, super, and state

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+      <h1 className='app-title'>Turing Cafe Reservations</h1>
+      <div className='resy-form'>
 
-        </div>
-        <div className='resy-container'>
-          
-        </div>
       </div>
+      <div className='resy-container'>
+
+      </div>
+    </div>
     )
   }
 }
