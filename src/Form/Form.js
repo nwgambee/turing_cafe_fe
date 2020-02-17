025222dbcb096a5517e10f6cfbd064b3cca25c5d
Noah Gambee <noah@gambee.dev>
2020-02-17 13:41:04 -0700

    Add imports and pseudocode

---
+++ src/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // name, date, time, number of guests
+    }
+  }
+}
+
+
+export default Form;
