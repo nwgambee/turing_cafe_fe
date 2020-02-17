53b92106fe28024207c33f3e60fef7679fe9b802
Noah Gambee <noah@gambee.dev>
2020-02-17 13:30:38 -0700

    Display reservation info on page through ResCard render

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const ReservationCard  = () => {
-console.log('res cards here ');
+const ReservationCard  = (props) => {
   return (
     <section className='res-card'>
+      <h3>{props.name}</h3>
+      <p>{props.date}</p>
+      <p>{props.time}</p>
+      <p>{props.number}</p>
     </section>
   )
 }
