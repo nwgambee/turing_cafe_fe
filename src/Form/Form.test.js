c912b434cf464d7e7a0a5fa5cf3f97176dea94a2
Noah Gambee <noah@gambee.dev>
2020-02-17 14:19:28 -0700

    Add Form component snapshot test

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './Form';
 import {shallow} from 'enzyme';
+
+it('should match the snapshot', () => {
+  let wrapper = shallow(<Form addReservation={jest.fn()} />)
+  expect(wrapper).toMatchSnapshot();
+})
