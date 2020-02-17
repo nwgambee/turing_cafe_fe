29db35768931e64477df48be03ffb1b8e087f2ac
Noah Gambee <noah@gambee.dev>
2020-02-17 15:31:25 -0700

    Add snapshot test for Container component

--- src/ReservationsContainer/ReservationContainer.test.js
+++ src/ReservationsContainer/ReservationContainer.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReservationContainer from './ReservationContainer';
 import {shallow} from 'enzyme';
+
+it('should match the snapshot', () => {
+  let wrapper = shallow(<ReservationContainer cancelReservation={jest.fn()} reservations={[]} />)
+  expect(wrapper).toMatchSnapshot()
+})
