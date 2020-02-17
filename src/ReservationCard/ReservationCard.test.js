2d320ba1e23fb1e5a370e1c0f00c8f9c18088c11
Noah Gambee <noah@gambee.dev>
2020-02-17 15:39:10 -0700

    Update Card tests

--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -4,7 +4,7 @@ import ReservationCard from './ReservationCard';
 import {shallow} from 'enzyme';
 
 it('should match the snapshot', () => {
-  let wrapper = shallow(<ReservationCard
+  const wrapper = shallow(<ReservationCard
     name='Noah'
     id={123}
     date='12/13'
@@ -13,4 +13,19 @@ it('should match the snapshot', () => {
     cancelReservation={jest.fn()}
     />)
   expect(wrapper).toMatchSnapshot();
+})
+
+it('should invoke cancelReservation when remove-res-btn is clicked', () => {
+  const wrapper = shallow(<ReservationCard
+    name='Noah'
+    id={123}
+    date='12/13'
+    time='3:45'
+    number={4}
+    cancelReservation={jest.fn()}
+    />)
+  wrapper.instance().cancelReservation = jest.fn();
+  wrapper.instance().forceUpdate();
+  wrapper.find('.remove-res-btn').simulate('click')
+  expect(wrapper.instance().cancelReservation).toHaveBeenCalled();
 })
